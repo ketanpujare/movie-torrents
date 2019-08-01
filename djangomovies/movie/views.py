df7d31db4e25c63d29d03659c1f0850970c62282
Ketan Pujare <ketanpujare@gmail.com>
2019-08-02 01:03:35 +0530

    added genre based search

--- djangomovies/movie/views.py
+++ djangomovies/movie/views.py
@@ -3,15 +3,23 @@ from django.shortcuts import render
 from django.contrib.auth.mixins     import LoginRequiredMixin
 from django.views.generic           import ListView
 
-from .models                        import Movie
+from .models                        import Movie, Genre
 
 # Create your views here.
 
 
 class HomeView(ListView):
-    paginate_by = 20
+    model = Movie
     ordering = ['-movie_year', 'movie_name']
     template_name = 'index.html'
-    queryset = Movie.objects.all()
+    paginate_by = 12
 
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['genres'] = Genre.objects.all()
+        return context
 
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset()
+        queryset = Genre.objects.filter(genre="Comedy")[0].movies.all()
+        return queryset
